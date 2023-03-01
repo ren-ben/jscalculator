682021263bb1ff0e161fcac4e41fee30424728a0
ben <oprzewlocki@student.tgm.ac.at>
2023-03-01 06:50:28 +0100

    Add School's Boilerplate
    Add the starting files

---
+++ functions.js
@@ -0,0 +1,93 @@
+/********************************************************************************/
+/* 
+					functions.js
+*/
+/********************************************************************************/
+
+
+/********************************************************************************/
+/*
+Function inZahlUmwandeln(wert)
+
+Beschreibung der Parameter:
+-> wert: Der Parameter wert enthält einen numerischen oder textuellen Wert 
+
+Beschreibung:
+Die Funktion versucht den mitgegebenen Wert in einen nummerischen Wert 
+umzuwandeln und nutzt hierfür die Funktion Number().
+Liefert diese Funktion den Wert NaN zurück wird eine entsprechende 
+alert() Meldung ausgegeben "Beim eingegebenen Wert handelt es sich nicht um eine
+Zahl oder Ziffer"
+*/
+/********************************************************************************/
+function inZahlUmwandeln(wert)
+{
+....
+
+
+return numwert;
+}
+
+/*
+Function berechne(wert1,wert2,operation)
+Beschreibung der Parameter:
+... Hier ausfüllen ...
+Beschreibung:
+... Hier ausfüllen ...
+*/
+/********************************************************************************/
+
+
+/*
+Function speicherInArray(wert1,wert2,operation, ergebnis)
+*/
+/********************************************************************************/
+/*
+Beschreibung der Parameter:
+... Hier ausfüllen ...
+Beschreibung:
+... Hier ausfüllen ...
+*/
+/********************************************************************************/
+
+
+
+/*
+Function neueRechnungOdUebersicht(neu)
+*/
+/********************************************************************************/
+/*
+Beschreibung der Parameter:
+... Hier ausfüllen ...
+*/
+/********************************************************************************/
+
+
+/*
+Function ausgabe()
+/********************************************************************************/
+/*
+Beschreibung der Parameter:
+... Hier ausfüllen ...
+*/
+/********************************************************************************/
+
+/*
+Einschränkung: Die Ausgabe kann auf viele verschiedene Arten erfolgen. 
+Ihr sollt eine der 3 folgenden auswählen:
+
+1) document.write(wert)
+Beispiel: document.write("<h2>Das Ergebnis lautet:</h2>"+wert);
+
+2) console.log(wert);
+Beispiele: console.log("Das Ergebnis lautet:"+wert); 
+
+3) alert(wert)
+Beispiel: alert("Das Ergebnis lautet:"+wert);
+
+Beschreibung der Parameter:
+... Hier ausfüllen ...
+Beschreibung:
+... Hier ausfüllen ...
+
+/********************************************************************************/
