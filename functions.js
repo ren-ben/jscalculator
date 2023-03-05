498f1810765b723f5fe6dece11f53b6f2fee8d0a
ben <OliGaming122@gmail.com>
2023-03-05 17:28:45 +0100

    add calculator & a loop
    add calculator functionality and put everything in a do while loop

--- functions.js
+++ functions.js
@@ -1,45 +1,4 @@
 /********************************************************************************/
-/* 
-					functions.js
-*/
-/********************************************************************************/
-
-/********************************************************************************/
-/*
-Function inZahlUmwandeln(wert)
-
-Beschreibung der Parameter:
--> wert: Der Parameter wert enthält einen numerischen oder textuellen Wert 
-
-Beschreibung:
-Die Funktion versucht den mitgegebenen Wert in einen nummerischen Wert 
-umzuwandeln und nutzt hierfür die Funktion Number().
-Liefert diese Funktion den Wert NaN zurück wird eine entsprechende 
-alert() Meldung ausgegeben "Beim eingegebenen Wert handelt es sich nicht um eine
-Zahl oder Ziffer"
-*/
-/********************************************************************************/
-/*
-function inZahlUmwandeln(wert)
-{
-....
-
-
-return numwert;
-}
-
-
-/********************************************************************************/
-/*
-Function berechne(wert1,wert2,operation)
-Beschreibung der Parameter:
-... Hier ausfüllen ...
-Beschreibung:
-... Hier ausfüllen ...
-*/
-/********************************************************************************/
-
-/********************************************************************************/
 /*
 Function speicherInHistoryArray(wert1,wert2,ergebnis)
 */
@@ -87,4 +46,83 @@ Beschreibung der Parameter:
 ... Hier ausfüllen ...
 Beschreibung:
 ... Hier ausfüllen ...
+
+3) Anzeige der gespeicherten Rechnungen
+Anschließend soll mit einer Abfrage und dem darüber eingegebenen Wert "o", eine Übersicht aller durchgeführten und gespeicherten Rechnungen angezeigt werden.
+
+Tipp zum auslesen des Arrays – for Schleife:
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let length = fruits.length;
+
+for(i=0;i<length;i++)
+
+{
+
+      alert(fruits[i]);
+
+}
+
+//Für mehrsimensionale Arrays müsst ihr dann zwei Zählvariablen definieren ... die länge der ersten erhaltet ihr wie gewohnt mit arrayname.length, die Länge der zweiten mittels arrayname[i].length
+
+Der Auszug aus dem Array - so soll in etwa es aussehen:
+
+#wert1#wert2#operation#ergebnis#
+
+#    1#    2#       plus#        3#
+
+#    1#    2#    minus#      -1#
+
+...
 */
+
+do {
+  var zahl1 = prompt("Bitte geben Sie eine Zahl ein: ");
+  var zahl2 = prompt("Bitte geben Sie eine Zahl ein: ");
+  var operator = prompt("Bitte geben Sie einen Operator ein: ");
+  var result = berechne[operator](ctn(zahl1), ctn(zahl2));
+  ausgabe(result);
+} while (operator != "o");
+
+function ctn(wert) {
+  var numwert = Number(wert);
+  if (isNaN(numwert)) {
+    alert(
+      "Beim eingegebenen Wert handelt es sich nicht um eine Zahl oder Ziffer"
+    );
+  }
+  return numwert;
+}
+
+function ausgabe(wert) {
+  document.write("<h2>Das Ergebnis lautet:</h2>" + wert);
+}
+
+function speicherInHistoryArray(wert1, wert2, ergebnis) {
+  var historyArray = [wert1, wert2, ergebnis];
+  return historyArray;
+}
+
+function speicherInMemoryArray(wert) {
+  var memoryArray = [wert];
+  return memoryArray;
+}
+
+var berechne = {
+  "+": function (x, y) {
+    return x + y;
+  },
+  "-": function (x, y) {
+    return x - y;
+  },
+  "*": function (x, y) {
+    return x * y;
+  },
+  "/": function (x, y) {
+    return x / y;
+  },
+  "%": function (x, y) {
+    return x % y;
+  },
+};
--- rechner.js
+++ rechner.js
@@ -1,24 +1,28 @@
 var zahl = prompt("Bitte geben Sie eine Zahl ein: ");
 var zahl2 = prompt("Bitte geben Sie eine Zahl ein: ");
 
+// Funktion um String in Zahl umzuwandeln, soll ctn heißen
+function ctn(x) {
+  return Number(x);
+}
+
 var math_it_up = {
   "+": function (x, y) {
-    return x + y;
+    return ctn(x) + ctn(y);
   },
   "-": function (x, y) {
-    return x - y;
+    return ctn(x) - ctn(y);
   },
   "*": function (x, y) {
-    return x * y;
+    return ctn(x) * ctn(y);
   },
   "/": function (x, y) {
-    return x / y;
+    return ctn(x) / ctn(y);
   },
   "%": function (x, y) {
-    return x % y;
+    return ctn(x) % ctn(y);
   },
 };
-
 var operator = prompt("Bitte geben Sie einen Operator ein: ");
 var result = math_it_up[operator](zahl, zahl2);
 
